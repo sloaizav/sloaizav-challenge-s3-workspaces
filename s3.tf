b4e6cb84d05da935a1b206bbea8a8ab9007a10ea
Sebastian Loaiza Velasquez <sebastian.loaiza@globant.com>
2022-07-07 21:59:51 -0500

    add s3 modules

--- s3.tf
+++ s3.tf
@@ -60,3 +60,69 @@ data "aws_iam_policy_document" "allow_access" {
     resources = ["arn:aws:s3:::${var.s3-bucket-name}/*"]
     }
 }
+
+/*modulos*/
+
+resource "aws_s3_bucket" "bucket_modules" {
+  
+}
+
+resource "aws_s3_bucket_acl" "acl_modules" {
+  bucket = aws_s3_bucket.bucket_modules.id
+  acl    = "public-read"
+
+}
+
+resource "aws_s3_bucket_versioning" "versioning_modules" {
+  bucket = aws_s3_bucket.bucket_modules.id
+  versioning_configuration {
+    status = var.s3-bucket-versioning
+  }
+}
+
+resource "aws_kms_key" "kmsKeyS3_modules" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encryption_modules" {
+ count = var.s3-bucket-encrypt == true ? 1 : 0
+
+  bucket = aws_s3_bucket.bucket_modules.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.kmsKeyS3_modules.arn 
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "policy_modules" {
+  bucket = aws_s3_bucket.bucket_modules.id
+  policy = data.aws_iam_policy_document.allow_access_modules.json
+}
+
+data "aws_iam_policy_document" "allow_access_modules" {
+  statement {
+    principals {
+      type        = "AWS"
+      identifiers = var.s3-bucket-allow_access_list
+    }
+
+    actions = ["s3:ListBucket"]
+
+    resources = [ "arn:aws:s3:::s3-sloaiza-modules"]
+    }
+    statement {
+    principals {
+      type        = "AWS"
+      identifiers = var.s3-bucket-allow_access_list
+    }
+
+    actions = ["s3:GetObject","s3:PutObject"]
+
+    resources = ["arn:aws:s3:::s3-sloaiza-modules/*"]
+    }
+}
